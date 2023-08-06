9cc3e8764ca90e0c1cfb18d65dc70acb34bfd643
MaiBoii <20183010@gs.cwnu.ac.kr>
2023-08-06 16:33:44 +0900

    teddy2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-use dotenv::dotenv;
-use std::env;
 mod ascii_arts;
 
+
 fn main() {
-    dotenv().ok();
+    println!("{}",ascii_arts::TITLE);
 
-    let safe_browse_api_key = env::var("SAFE_BROWSE_API_KEY").expect("SAFE_BROWSE_API_KEY not set");
 
-    println!("API_KEY: {}", safe_browse_api_key);
-    println!("Title: {}", ascii_arts::TITLE);
 }
